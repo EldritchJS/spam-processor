39f1b3947bf2917d1c6e0c6dee29daad652e194b
William Benton <self@willbenton.com>
2019-08-16 08:53:08 -0400

    removed flask config

--- app.py
+++ app.py
@@ -62,21 +62,6 @@ def main(args):
 
 
 if __name__ == '__main__':
-    lconfig.dictConfig({
-        'version': 1,
-        'formatters': {'default': {
-            'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
-            }},
-        'handlers': {'wsgi': {
-            'class': 'logging.StreamHandler',
-            'stream': 'ext://flask.logging.wsgi_errors_stream',
-            'formatter': 'default'
-            }},
-        'root': {
-            'level': 'INFO',
-            'handlers': ['wsgi']
-            }
-        })
     logging.basicConfig(level=logging.INFO)
     logging.info('starting spam-processor')
     parser = argparse.ArgumentParser(
