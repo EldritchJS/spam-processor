fc2b9f12fb25b57c248fe8342c7ca82194c5ee8b
William Benton <self@willbenton.com>
2020-02-18 10:10:28 -0600

    miscellaneous cleanups
    1.  loop around connecting to Kafka broker
    2.  pyflakes cleanliness

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import argparse
 import logging
-import logging.config as lconfig
+
 import os
 import threading
 
@@ -17,20 +17,31 @@ DEFAULT_BASE_URL = "http://pipeline:8080/%s"
 
 
 def score_text(text, url=None):
-    url = (url or (DEFAULT_BASE_URL % "predict"))
+    url = url or (DEFAULT_BASE_URL % "predict")
     if type(text) == str:
         text = [text]
     payload = urlencode({"json_args": json.dumps(text)})
-    headers = {'content-type': 'application/x-www-form-urlencoded'}
+    headers = {"content-type": "application/x-www-form-urlencoded"}
     response = requests.request("POST", url, data=payload, headers=headers)
-    return None
+    return response.text
 
 
 def consumer(args):
-    logging.info('starting kafka consumer')
-    consumer = kafka.KafkaConsumer(args.topic, bootstrap_servers=args.brokers)
+    attempts = 1
+    ready = False
+    kc = None
+
+    while not ready:
+        logging.info("starting kafka consumer (attempt %d)" % attempts)
+        attempts += 1
+        try:
+            kc = kafka.KafkaConsumer(args.topic, bootstrap_servers=args.brokers)
+            ready = True
+        finally:
+            pass
+
     msg_count = 0
-    for msg in consumer:
+    for msg in kc:
         if exit_event.is_set():
             logging.info("exiting upon request")
             break
@@ -38,20 +49,20 @@ def consumer(args):
             msg_count = msg_count + 1
             if msg_count % 100 == 0:
                 logging.info("scoring message %d" % msg_count)
-            score_text(json.loads(str(msg.value, 'utf-8'))["text"])
+            score_text(json.loads(str(msg.value, "utf-8"))["text"])
         except Exception as e:
             logging.error(e.message)
-    logging.info('exiting kafka consumer')
+    logging.info("exiting kafka consumer")
 
 
 def get_arg(env, default):
-    return os.getenv(env) if os.getenv(env, '') is not '' else default
+    return os.getenv(env) if os.getenv(env, "") != "" else default
 
 
 def parse_args(parser):
     args = parser.parse_args()
-    args.brokers = get_arg('KAFKA_BROKERS', args.brokers)
-    args.topic = get_arg('KAFKA_TOPIC', args.topic)
+    args.brokers = get_arg("KAFKA_BROKERS", args.brokers)
+    args.topic = get_arg("KAFKA_TOPIC", args.topic)
     return args
 
 
@@ -63,22 +74,23 @@ def main(args):
 
     # exit_event.set()
     cons.join()
-    logging.info('exiting spam-processor')
+    logging.info("exiting spam-processor")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
-    logging.info('starting spam-processor')
-    parser = argparse.ArgumentParser(
-            description='listen for some stuff on kafka')
+    logging.info("starting spam-processor")
+    parser = argparse.ArgumentParser(description="listen for some stuff on kafka")
     parser.add_argument(
-            '--brokers',
-            help='The bootstrap servers, env variable KAFKA_BROKERS',
-            default='kafka.kafka.svc:9092')
+        "--brokers",
+        help="The bootstrap servers, env variable KAFKA_BROKERS",
+        default="kafka.kafka.svc:9092",
+    )
     parser.add_argument(
-            '--topic',
-            help='Topic to read from, env variable KAFKA_TOPIC',
-            default='social-firehose')
+        "--topic",
+        help="Topic to read from, env variable KAFKA_TOPIC",
+        default="social-firehose",
+    )
     args = parse_args(parser)
     main(args)
-    logging.info('exiting')
+    logging.info("exiting")
