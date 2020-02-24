b84d928f86d7795eabc9560497df14d053ced08c
William Benton <self@willbenton.com>
2020-02-24 08:52:16 -0600

    flake-related cleanups

--- app.py
+++ app.py
@@ -4,21 +4,20 @@ import logging
 import os
 import threading
 
-import kafka
-import requests
-
-exit_event = threading.Event()
-
 from urllib.parse import urlencode
 import json
 
+import requests
+import kafka
+
+EXIT_EVENT = threading.Event()
 
 DEFAULT_BASE_URL = "http://pipeline:8080/%s"
 
 
 def score_text(text, url=None):
     url = url or (DEFAULT_BASE_URL % "predict")
-    if type(text) == str:
+    if isinstance(text, str):
         text = [text]
     payload = urlencode({"json_args": json.dumps(text)})
     headers = {"content-type": "application/x-www-form-urlencoded"}
@@ -29,20 +28,20 @@ def score_text(text, url=None):
 def consumer(args):
     attempts = 1
     ready = False
-    kc = None
+    kafka_consumer = None
 
     while not ready:
         logging.info("starting kafka consumer (attempt %d)" % attempts)
         attempts += 1
         try:
-            kc = kafka.KafkaConsumer(args.topic, bootstrap_servers=args.brokers)
+            kafka_consumer = kafka.KafkaConsumer(args.topic, bootstrap_servers=args.brokers)
             ready = True
         finally:
             pass
 
     msg_count = 0
-    for msg in kc:
-        if exit_event.is_set():
+    for msg in kafka_consumer:
+        if EXIT_EVENT.is_set():
             logging.info("exiting upon request")
             break
         try:
@@ -50,8 +49,8 @@ def consumer(args):
             if msg_count % 100 == 0:
                 logging.info("scoring message %d" % msg_count)
             score_text(json.loads(str(msg.value, "utf-8"))["text"])
-        except Exception as e:
-            logging.error(e.message)
+        except Exception as ex:
+            logging.error(repr(ex))
     logging.info("exiting kafka consumer")
 
 
@@ -67,7 +66,7 @@ def parse_args(parser):
 
 
 def main(args):
-    exit_event.clear()
+    EXIT_EVENT.clear()
     # setup consumer thread
     cons = threading.Thread(group=None, target=consumer, args=(args,))
     cons.start()
@@ -80,17 +79,17 @@ def main(args):
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
     logging.info("starting spam-processor")
-    parser = argparse.ArgumentParser(description="listen for some stuff on kafka")
-    parser.add_argument(
+    PARSER = argparse.ArgumentParser(description="listen for some stuff on kafka")
+    PARSER.add_argument(
         "--brokers",
         help="The bootstrap servers, env variable KAFKA_BROKERS",
         default="kafka.kafka.svc:9092",
     )
-    parser.add_argument(
+    PARSER.add_argument(
         "--topic",
         help="Topic to read from, env variable KAFKA_TOPIC",
         default="social-firehose",
     )
-    args = parse_args(parser)
-    main(args)
+    cmdline_args = parse_args(PARSER)
+    main(cmdline_args)
     logging.info("exiting")
